7bd7c465319d9c9c97ba1e992e4b7b482a181513
maddenlockin <maddenlockin@gmail.com>
2021-05-12 17:25:39 -0700

    close to checkout button requirements but with an error

--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,43 @@
 import hrtProducts from '../data/hrt.js';
 import { renderProduct } from '../products/render-product.js';
-
+import { findById } from '../cart/utils.js';
+//import { renderCart, getCartTotal } from '../cart/render-cart.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
+test('render product function', (expect) => {
     const expected = 	
     '<li class="estrogen" title="pill form of estrogen"><h3>Estradiol Pills</h3><img src="../assets/../assets/estradiol-pills.png" alt="Estradiol Pills image"><p class="price">$59.00<button value="1">Add to Cart</button></p></li>';
-    //Act 
-    // Call the function you're testing and set the result to a const
     const actual = renderProduct(hrtProducts[0]).outerHTML;
-
-    //Expect
-    // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+test('findById will return the expected id', (expect) => {
+    const testosteroneCypionate = [{
+        id: 4,
+        name: 'Testosterone Cypionate',
+        img: '../assets/testosterone-cypionate.png',
+        description: 'most common injectable form of testosterone',
+        category: 'testosterone',
+        price: 59.00,
+        cost: 0.25,
+    }];
+    const expected = {
+        id: 4,
+        name: 'Testosterone Cypionate',
+        img: '../assets/testosterone-cypionate.png',
+        description: 'most common injectable form of testosterone',
+        category: 'testosterone',
+        price: 59.00,
+        cost: 0.25,
+    };
+    const actual = findById(testosteroneCypionate, 4);
+    expect.deepEqual(actual, expected);
+
+});
+
+// test('get cart total function', (expect) => {
+//     const expected = 76;
+//     const actual = renderCart(59, 17);
+//     expect.deepEqual(actual, expected);
+// });
